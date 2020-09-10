f1b8fb7be2ac166211d0009780ce3580888d018f
Jasmin Suljic <jasmin.suljich@gmail.com>
2020-09-10 13:23:50 +0200

    CustomerParams properties are now nullable

--- Monri/Classes/CustomerParams.swift
+++ Monri/Classes/CustomerParams.swift
@@ -9,15 +9,15 @@ import Foundation
 
 public class CustomerParams {
 
-    let email: String
-    let fullName: String
-    let address: String
-    let city: String
-    let zip: String
-    let phone: String
-    let country: String
+    let email: String?
+    let fullName: String?
+    let address: String?
+    let city: String?
+    let zip: String?
+    let phone: String?
+    let country: String?
 
-    public init(email: String, fullName: String, address: String, city: String, zip: String, phone: String, country: String) {
+    public init(email: String?, fullName: String?, address: String?, city: String?, zip: String?, phone: String?, country: String?) {
         self.email = email
         self.fullName = fullName
         self.address = address
