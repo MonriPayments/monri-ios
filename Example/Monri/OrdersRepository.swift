3723865bfb1cc3a52b25d05cacf1b477757b24b9
Marin Mikulec <mikulec.marin52@gmail.com>
2026-08-21 13:36:37 +0200

    fix: send signed request body verbatim in createPayment
    Build the /v2/payment/new request as a URLRequest with httpBody set to
    the exact bytes used to compute the WP3-v2 digest, instead of passing the
    parameters dict for Alamofire to re-encode. This guarantees the signed
    body matches the transmitted body, avoiding signature mismatches.
    
    Applied to the Example app (OrdersRepository) and the two API test
    helpers.
    
    SCDELMON-32

--- Example/Monri/OrdersRepository.swift
+++ Example/Monri/OrdersRepository.swift
@@ -59,11 +59,19 @@ public class OrdersRepository {
 
         let url = "https://ipgtest.monri.com/v2/payment/new"
 
-        AF.request(url,
-                   method: .post,
-                   parameters: parameters,
-                   encoding: JSONEncoding.default,
-                   headers: headers)
+        guard let requestUrl = URL(string: url) else {
+            callback(nil)
+            return
+        }
+
+        var request = URLRequest(url: requestUrl)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.httpBody = bodyData
+        for (name, value) in headers.dictionary {
+            request.setValue(value, forHTTPHeaderField: name)
+        }
+
+        AF.request(request)
         .responseData { dataResponse in
             guard let data = dataResponse.data else {
                 callback(nil)
--- Example/Tests/MonriPaymentsCustomerApiTest.swift
+++ Example/Tests/MonriPaymentsCustomerApiTest.swift
@@ -89,11 +89,19 @@ final class MonriPaymentsCustomerApiTest: XCTestCase {
 
         let url = "https://ipgtest.monri.com/v2/payment/new"
 
-        AF.request(url,
-                   method: .post,
-                   parameters: parameters,
-                   encoding: JSONEncoding.default,
-                   headers: headers)
+        guard let requestUrl = URL(string: url) else {
+            callback(nil, nil)
+            return
+        }
+
+        var request = URLRequest(url: requestUrl)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.httpBody = bodyData
+        for (name, value) in headers.dictionary {
+            request.setValue(value, forHTTPHeaderField: name)
+        }
+
+        AF.request(request)
         .responseData { dataResponse in
             guard let data = dataResponse.data else {
                 callback(nil, nil)
